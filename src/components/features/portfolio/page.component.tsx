4a80461bb7a2bd0b389cef87c0c1d4da063068fd
Luc Castelain <luc.castelain@icloud.com>
2026-02-15 00:02:12 +0100

    Added event listener for filters and menu in mobile screen.

--- src/components/features/portfolio/page.component.tsx
+++ src/components/features/portfolio/page.component.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {ReactElement, useEffect, useState} from "react";
+import {ReactElement, useCallback, useEffect, useRef, useState} from "react";
 import ProjectCardComponent from "@/components/ui/project-card.component";
 import {getPortfolioData} from "@/data/utils/get-data";
 import {getText} from "@/data/utils/get-text";
@@ -15,17 +15,26 @@ export default function PortfolioPageComponent({language}: Readonly<PortfolioPag
     const [softwareFilter, setSoftwareFilter] = useState<string>(data.software.ALL);
     const [programmingLanguageFilter, setProgrammingLanguageFilter] = useState<string>(data.programmingLanguages.ALL);
     const [showFilters, setShowFilters] = useState<boolean>(false);
+    const filtersRef = useRef<HTMLDivElement>(null);
+
+    const closeDropdown = useCallback((e: MouseEvent): void => {
+        if(showFilters && !filtersRef.current?.contains(e.target as Node)) {
+            setShowFilters(false);
+        }
+    }, [showFilters]);
 
     useEffect(() => {
-
-    }, []);
+        document.addEventListener("click", closeDropdown);
+        return (): void => {document.removeEventListener("click", closeDropdown);};
+    }, [closeDropdown]);
 
     if(!data.projectTypes) return <div>loading...</div>
 
     return (
         <div className="size-full flex flex-col">
             <div className={twMerge("md:hidden absolute w-full h-[calc(100%-3rem)] z-10 bg-gray-900 dark:bg-neutral-800 opacity-50", !showFilters && "hidden")}/>
-            <div className={twMerge("absolute left-0 z-20 h-[calc(100%-3rem)] md:h-fit w-fit md:w-full bg-white dark:bg-neutral-900 dark:text-gray-100 md:bg-transparent md:z-0 md:static flex flex-col md:flex-row p-2 md:p-0 md:my-2 gap-4 transition-all -translate-x-full md:translate-x-0", showFilters && "translate-x-0")}>
+            <div ref={filtersRef}
+                 className={twMerge("absolute left-0 z-20 h-[calc(100%-3rem)] md:h-fit w-fit md:w-full bg-white dark:bg-neutral-900 dark:text-gray-100 md:bg-transparent md:z-0 md:static flex flex-col md:flex-row p-2 md:p-0 md:my-2 gap-4 transition-all -translate-x-full md:translate-x-0", showFilters && "translate-x-0")}>
                 <button onClick={() => setShowFilters(false)}
                         className="md:hidden md:pointer-events-none size-fit p-2 rounded-lg hover:bg-neutral-200 dark:hover:bg-neutral-800">
                     <HiMiniArrowLeft className="size-5"/>
--- src/components/ui/navbar.component.tsx
+++ src/components/ui/navbar.component.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {ReactElement, useState} from "react";
+import {ReactElement, useCallback, useEffect, useRef, useState} from "react";
 import {DarkModeToggleComponent} from "@/components/ui/dark-mode/dark-mode-toggle.component";
 import LanguageDropdownComponent from "@/components/ui/languages/language-dropdown.component";
 import Link from "next/dist/client/link";
@@ -14,7 +14,21 @@ export default function NavbarComponent(): ReactElement {
     const params = useSearchParams();
     const language = params.get("lang") ?? "en";
     const text = getText(language);
+
     const [openMenu, setOpenMenu] = useState<boolean>(false);
+    const menuButtonRef = useRef<HTMLButtonElement>(null);
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    const closeDropdown = useCallback((e: MouseEvent): void => {
+        if(openMenu && !menuRef.current?.contains(e.target as Node) && !menuButtonRef.current?.contains(e.target as Node)) {
+            setOpenMenu(false);
+        }
+    }, [openMenu]);
+
+    useEffect(() => {
+        document.addEventListener("click", closeDropdown);
+        return (): void => {document.removeEventListener("click", closeDropdown);};
+    }, [closeDropdown]);
 
     return (
         <nav className={twMerge("absolute top-0 z-50 w-full h-fit bg-white border-gray-200 dark:border-neutral-800 dark:bg-neutral-900 border-b md:shadow-none", openMenu && "shadow-md")}>
@@ -23,13 +37,15 @@ export default function NavbarComponent(): ReactElement {
                     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900 dark:text-white">Luc Castelain</span>
                     </a>
-                    <button className="ml-auto p-2 md:hidden md:pointer-events-none rounded-lg hover:bg-neutral-200 dark:text-gray-100 dark:hover:bg-neutral-800"
+                    <button ref={menuButtonRef}
+                            className="ml-auto p-2 md:hidden md:pointer-events-none rounded-lg hover:bg-neutral-200 dark:text-gray-100 dark:hover:bg-neutral-800"
                             onClick={() => setOpenMenu(!openMenu)}>
                         <FaBars className="size-5"/>
                     </button>
                 </div>
 
-                <div className={twMerge("w-full flex flex-col md:flex-row origin-top transition-all h-0 scale-y-0 md:h-fit md:scale-y-100", openMenu && "h-fit scale-y-100")}>
+                <div ref={menuRef}
+                     className={twMerge("w-full flex flex-col md:flex-row origin-top transition-all h-0 scale-y-0 md:h-fit md:scale-y-100", openMenu && "h-fit scale-y-100")}>
                     <div className="items-center justify-between w-full text-center md:w-fit md:mx-auto md:flex">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-neutral-800 md:dark:bg-neutral-900 dark:border-gray-700">
                             <li>
