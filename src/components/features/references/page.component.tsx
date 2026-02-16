33445ea24a3ddb241b0a8c0f07556a2f599a5f85
Luc <luc.castelain@baudinchateauneuf.com>
2026-02-16 17:07:35 +0100

    Updated classes.

--- src/components/features/references/page.component.tsx
+++ src/components/features/references/page.component.tsx
@@ -7,37 +7,37 @@ export default function ReferencesPageComponent({language}: Readonly<ReferencesP
     const text = getText(language);
 
     return (
-        <div className="size-full flex flex-col items-center p-6">
+        <div className="size-full flex flex-col max-w-7xl mx-auto items-center p-6">
             <h3 className="mx-auto text-2xl font-bold text-sky-500 mb-5">{text.references}</h3>
-            <div className="flex flex-col max-w-7xl mx-auto size-full text-center gap-y-4 overflow-y-auto">
+            <div className="flex flex-col size-full text-center px-6 gap-y-4 overflow-y-auto">
                 <TextWithLineBreaksComponent text={text.referencesIHave}
                                              className="text-left"/>
-                <div className="size-full flex flex-wrap justify-around">
+                <div className="size-full grid grid-cols-1 md:grid-cols-2 md:gap-6">
                     <a href="/fiu_reference.pdf"
                        target="_blank"
                        rel="noopeneexitr noreferrer"
-                       className="group h-fit p-3 rounded-lg transition-all hover:scale-[1.02] hover:bg-neutral-100 dark:hover:bg-neutral-800">
+                       className="group mx-auto h-fit p-3 rounded-lg transition-all hover:scale-[1.02] hover:bg-neutral-100 dark:hover:bg-neutral-800">
                         <figure>
                             <Image src="/static/references/fiu_reference.webp"
                                    alt="FIU reference"
                                    width={500}
                                    height={0}
                                    style={{width: "auto", height: "100%"}}
-                                   className="transition-all md:max-w-96 border group-hover:border-0 dark:border-0"/>
+                                   className="transition-all border group-hover:border-0 dark:border-0"/>
                             <figcaption>{text.fiuReference}</figcaption>
                         </figure>
                     </a>
                     <a href="/dosisoft_reference.pdf"
                        target="_blank"
                        rel="noopener noreferrer"
-                       className="group h-fit p-3 rounded-lg transition-all hover:scale-[1.02] hover:bg-neutral-100 dark:hover:bg-neutral-800">
+                       className="group mx-auto h-fit p-3 rounded-lg transition-all hover:scale-[1.02] hover:bg-neutral-100 dark:hover:bg-neutral-800">
                         <figure>
                             <Image src="/static/references/dosisoft_reference.webp"
                                    alt="Dosisoft reference"
                                    width={500}
                                    height={0}
                                    style={{width: "auto", height: "100%"}}
-                                   className="transition-all md:max-w-96 border group-hover:border-0 dark:border-0"/>
+                                   className="transition-all border group-hover:border-0 dark:border-0"/>
                             <figcaption>{text.dosisoftReference}</figcaption>
                         </figure>
                     </a>
